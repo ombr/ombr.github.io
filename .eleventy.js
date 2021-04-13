da9c61494fde2e0c38965c272f9869f74fad1714
Luc Boissaye <luc@boissaye.fr>
2021-04-13 13:42:15 -0400

    WIP with tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,2 @@
-const CleanCSS = require("clean-css");
-const sass = require('sass');
-
-code = `h1
-  font-size: 40px
-`
-console.log(sass.renderSync({ data: code, indentedSyntax: true }).css.toString())
 module.exports = function(eleventyConfig) {
-  eleventyConfig.setPugOptions({
-    filters: {
-      sass: function(code, options) {
-        return new CleanCSS({}).minify(
-          sass.renderSync({
-            data: code,
-            indentedSyntax: true
-          }).css.toString()
-        ).styles;
-      }
-    }
-  });
 };
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./*.pug'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  separator: '_',
+}
