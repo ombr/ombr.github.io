aa4fd6d4394806457fce2200141baf2fc7b8005a
Luc Boissaye <luc@boissaye.fr>
2021-04-14 16:12:13 -0400

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,42 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sans: [
+        'Ubuntu',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      serif: ['Lora', 'ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      mono: [
+        'ui-monospace',
+        'SFMono-Regular',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace',
+      ],
+    }
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+   require('@tailwindcss/typography'),
+  ],
   separator: '_',
 }
