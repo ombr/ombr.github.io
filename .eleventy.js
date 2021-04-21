820222639ff97df97a2817b7b368f51734154c7c
Luc Boissaye <luc@boissaye.fr>
2021-04-20 21:29:19 -0400

    WIP try to add a simple image

--- .eleventy.js
+++ .eleventy.js
@@ -1,2 +1,22 @@
+const Image = require("@11ty/eleventy-img");
+my_image = function(src, alt, sizes) {
+  let options = {
+    widths: sizes,
+    formats: ["avif", "webp", "jpeg"],
+    outputDir: '_site/img'
+  }
+  Image(src, options)
+  return Image.generateHTML(Image.statsSync(src, options),
+    {
+      alt: alt,
+      sizes: sizes,
+      loading: "lazy",
+      decoding: "async",
+    }
+  )
+}
 module.exports = function(eleventyConfig) {
+  eleventyConfig.setPugOptions({
+    globals: ['my_image']
+  });
 };
